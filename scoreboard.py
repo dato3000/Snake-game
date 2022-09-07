dffa7a4c2966b4df382d64a9c4a8383cc33769c3
dato3000 <94035318+dato3000@users.noreply.github.com>
2022-09-06 20:51:28 -0400

    Add files via upload

---
+++ scoreboard.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+from food import Food
+
+food = Food()
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.write(f"Your Score {self.score}", align="center", font=("Arial", 24, "normal"))
+        self.hideturtle()
+
+    def increase_score(self):
+        self.score += 1
+        self.write(f"Your Score {self.score}", align="center", font=("Arial", 24, "normal"))
